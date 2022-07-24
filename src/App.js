2a6cf16fc69962513ad52e4ce3cc896cecb94c71
vokenad <vefe44@gmail.com>
2022-07-24 19:19:44 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ParallaxProvider } from "react-scroll-parallax";
 import ReactGA from "react-ga4";
 import Navigation from "./routes/index";
 import "./App.css";
@@ -12,9 +11,7 @@ const App = () => {
 
   return (
     <>
-      <ParallaxProvider>
-        <Navigation />
-      </ParallaxProvider>
+      <Navigation />
     </>
   );
 };
