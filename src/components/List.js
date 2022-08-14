35bb405111ff783c2882fce4de2795517c65fbe4
vokenad <vefe44@gmail.com>
2022-08-14 22:16:21 +0100

    whatsapp share

--- src/components/List.js
+++ src/components/List.js
@@ -214,7 +214,28 @@ const List = ({
               disabled={isShareLoading}
               onClick={handleShareList}
             >
-              Share with a Dropper On Whatsapp
+              {isShareLoading ? (
+                <svg
+                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="#fff"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    fill="#888888"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              ) : (
+                "Share with a Dropper On Whatsapp"
+              )}
             </button>
           </div>
         </div>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -111,6 +111,13 @@ const ShoppingList = () => {
     const result = sessionStorage.getItem("auth");
     const { token, data } = JSON.parse(result);
 
+    const whatsAppLink = `https://wa.me/+2349061494881?text=Yo ! I’d love to schedule a Dropp for my shopping list ${JSON.stringify(
+      {
+        name: listName,
+        items,
+      }
+    )}`;
+
     axios
       .post(
         `https://dropp-backend.herokuapp.com/api/v1/shoppingList/shopper/${data?.user?._id}`,
@@ -122,13 +129,15 @@ const ShoppingList = () => {
         }
       )
       .then(({ data }) => {
-        alert("List saved.");
+        window.open(whatsAppLink);
         handleSelectList("my");
         getUserLists();
       })
       .catch((error) => {})
       .finally(() => {
-        setListName();
+        setOpenList("");
+        setListId("");
+        setListName("");
         setItems([]);
         setIsShareLoading(false);
       });
@@ -342,7 +351,28 @@ const ShoppingList = () => {
                 disabled={isShareLoading}
                 onClick={handleShareList}
               >
-                Share with a Dropper On Whatsapp
+                {isShareLoading ? (
+                  <svg
+                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="#fff"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      fill="#888888"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                ) : (
+                  "Share with a Dropper On Whatsapp"
+                )}
               </button>
             </div>
           </div>
