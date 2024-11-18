33464d2973411862b9ba4a927ee1a41331a90983
Udonne Chinonso <nonso.udonne@gmail.com>
2024-11-18 13:24:13 +0100

    merge main man

--- src/pages/waiter/WaiterDashboard.tsx
+++ src/pages/waiter/WaiterDashboard.tsx
@@ -361,7 +361,6 @@ const WaiterDashboard = () => {
                           openOrdersModal={() => openOrdersModal(tableOrder)}
                           closeOrdersModal={() => closeOrdersModal()}
                           getTableOrders={getTableOrders}
-                          selectedCategory={selectedCategory?.value}
                           chef={chef}
                           waiter={waiter}
                         />)
