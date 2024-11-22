dda3b8e288b51e4eeaa8cb000a39a55968694941
Voke <vefe44@gmail.com>
2024-11-23 00:31:08 +0100

    analytics set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
 import ReactGA from "react-ga4";
+import TagManager from "react-gtm-module";
+import Hotjar from "@hotjar/browser";
 import AppRoutes from "./routes";
-// import AppRoutes from "./router";
 
-// declare global {
-//   interface Window {
-//     dataLayer: any[];
-//   }
-// }
-
+const tagManagerArgs = {
+  gtmId: import.meta.env.VITE_GTM_ID || "",
+};
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(import.meta.env.VITE__GA_ID || "e2");
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+    });
+    TagManager.initialize(tagManagerArgs);
+    Hotjar.init(
+      Number(import.meta.env.VITE_HOTJAR_SITE_ID),
+      Number(import.meta.env.VITE__HOTJAR_VERSION)
+    );
+  }, []);
+
   return <AppRoutes />;
 }
 
