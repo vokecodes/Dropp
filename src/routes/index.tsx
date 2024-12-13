0c77929ea91f13948b85da2e47c591721ac37e71
Voke <vefe44@gmail.com>
2024-12-13 11:20:54 +0100

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  Routes,
-  Route,
-  BrowserRouter,
-} from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { connect, shallowEqual, useSelector } from "react-redux";
 import Home from "../pages/dropp-main/Home";
 import AuthRoutes from "./authRoutes";
@@ -21,7 +17,14 @@ import {
   WAITER_ROUTES,
 } from "./routes";
 import NotFound from "../components/NotFound";
-import { ADMIN_USER, CHEF_USER, COMPANY_USER, QSR_SUB_ADMIN_USER, SUB_CHEF_USER } from "../config/UserType";
+import {
+  ADMIN_USER,
+  CHEF_USER,
+  COMPANY_USER,
+  QSR_SUB_ADMIN_USER,
+  QUICK_SERVICE_USER,
+  SUB_CHEF_USER,
+} from "../config/UserType";
 import Explore from "../pages/customer/Explore";
 import ChefShop from "../pages/chef/ChefShop";
 import RestaurantShop from "../pages/chef/RestaurantShop";
@@ -41,7 +44,11 @@ import CustomerRoutes from "../pages/customer/Routes";
 import SubChefRoutes from "../pages/sub-chef/Routes";
 import CustomerEventSignUp from "../pages/events/SignUp";
 import AdminRoutes from "../pages/dashboard/Routes";
-import { QsrRoutes, CashierRoutes, QsrSubAdminRoutes } from "../pages/qsr/Routes";
+import {
+  QsrRoutes,
+  CashierRoutes,
+  QsrSubAdminRoutes,
+} from "../pages/qsr/Routes";
 import QsrShop from "../pages/qsr/QsrShop";
 
 const AppRoutes = () => {
@@ -63,10 +70,7 @@ const AppRoutes = () => {
           path={HOME_ROUTES.exploreRestaurant}
           element={<RestaurantShop />}
         />
-        <Route
-          path={HOME_ROUTES.exploreQsr}
-          element={<QsrShop />}
-        />
+        <Route path={HOME_ROUTES.exploreQsr} element={<QsrShop />} />
         <Route
           path={HOME_ROUTES.exploreRestaurantEdit}
           element={<RestaurantShop />}
@@ -74,16 +78,16 @@ const AppRoutes = () => {
         <Route
           path={HOME_ROUTES.chefLandingPage}
           element={<ChefLandingPage />}
-          />
+        />
         <Route
           path={HOME_ROUTES.companies}
           element={<CompaniesLandingPage />}
-          />
+        />
         <Route path={HOME_ROUTES.previewChef} element={<PreviewChefShop />} />
         <Route
           path={HOME_ROUTES.previewRestaurant}
           element={<PreviewRestaurantShop />}
-          />
+        />
         <Route path={HOME_ROUTES.events} element={<Events />} />
         <Route path={HOME_ROUTES.foodSafety} element={<FoodSafety />} />
         <Route path={HOME_ROUTES.privacyPolicy} element={<PrivacyPolicy />} />
@@ -93,55 +97,52 @@ const AppRoutes = () => {
         <Route path={WAITER_ROUTES.waiter} element={<WaiterRoutes />} />
 
         <Route path={CASHIER_ROUTES.cashier} element={<CashierRoutes />} />
-        
-          {user?.user ? (
-            <>
-              {user?.user?.userType === CHEF_USER ? (
-                <>
-                  {user?.user?.chefType === 'quick_service' ? (
-                    <Route path={QSR_ROUTES.qsr} element={<QsrRoutes />} />
-                  ) : (
-                    <Route path={CHEF_ROUTES.chef} element={<ChefRoutes />} />
-                  )}
-                </>
-              ) : user?.user?.userType === SUB_CHEF_USER ? (
-                <Route
-                  path={SUB_CHEF_ROUTES.subChef}
-                  element={<SubChefRoutes />}
-                />
-              ) : user?.user?.userType === QSR_SUB_ADMIN_USER ? (
-                <Route
-                  path={QSR_SUBADMIN_ROUTES.qsrSubAdmin}
-                  element={<QsrSubAdminRoutes />}
-                />
-              ) : user?.user?.userType === COMPANY_USER ? (
-                <Route
-                  path={COMPANY_ROUTES.company}
-                  element={<CompanyRoutes />}
-                />
-              ) : user?.user?.userType === ADMIN_USER ? (
-                <Route
-                  path={ADMIN_ROUTES.admin}
-                  element={<AdminRoutes />}
-                />
-              ) : (
-                <>
-                  <Route
-                    path={CUSTOMER_ROUTES.customer}
-                    element={<CustomerRoutes />}
-                  />
-                </>
-              )}
-            </>
-          ) : (
-            <>
+
+        {user?.user ? (
+          <>
+            {user?.user?.userType === CHEF_USER ? (
+              <>
+                {user?.user?.chefType === QUICK_SERVICE_USER ? (
+                  <Route path={QSR_ROUTES.qsr} element={<QsrRoutes />} />
+                ) : (
+                  <Route path={CHEF_ROUTES.chef} element={<ChefRoutes />} />
+                )}
+              </>
+            ) : user?.user?.userType === SUB_CHEF_USER ? (
               <Route
-                path={HOME_ROUTES.eventRegister}
-                element={<CustomerEventSignUp />}
+                path={SUB_CHEF_ROUTES.subChef}
+                element={<SubChefRoutes />}
               />
-              <Route path={AUTH_ROUTES.auth} element={<AuthRoutes />} />
-            </>
-          )}
+            ) : user?.user?.userType === QSR_SUB_ADMIN_USER ? (
+              <Route
+                path={QSR_SUBADMIN_ROUTES.qsrSubAdmin}
+                element={<QsrSubAdminRoutes />}
+              />
+            ) : user?.user?.userType === COMPANY_USER ? (
+              <Route
+                path={COMPANY_ROUTES.company}
+                element={<CompanyRoutes />}
+              />
+            ) : user?.user?.userType === ADMIN_USER ? (
+              <Route path={ADMIN_ROUTES.admin} element={<AdminRoutes />} />
+            ) : (
+              <>
+                <Route
+                  path={CUSTOMER_ROUTES.customer}
+                  element={<CustomerRoutes />}
+                />
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <Route
+              path={HOME_ROUTES.eventRegister}
+              element={<CustomerEventSignUp />}
+            />
+            <Route path={AUTH_ROUTES.auth} element={<AuthRoutes />} />
+          </>
+        )}
 
         <Route path="*" element={<NotFound />} />
       </Routes>
