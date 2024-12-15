ac6f781c9e7b26e0edc2f9bbaf9d858766f6d971
Udonne Chinonso <nonso.udonne@gmail.com>
2024-12-15 14:54:41 +0100

    sub

--- src/_redux/user/userCrud.ts
+++ src/_redux/user/userCrud.ts
@@ -213,10 +213,11 @@ export const getRestaurantSubChefDashboardCrud = (
   toDate = "",
   payment = "",
   section = "",
-  table = ""
+  table = "",
+  breakdownOption = ""
 ) => {
   return SERVER.get(
-    `${SUB_CHEF_URL}/dashboard?fromDate=${fromDate}&toDate=${toDate}&payment=${payment}&section=${section}&table=${table}`
+    `${SUB_CHEF_URL}/dashboard?fromDate=${fromDate}&toDate=${toDate}&payment=${payment}&section=${section}&table=${table}&breakdownOption=${breakdownOption}`
   );
 };
 
@@ -283,15 +284,29 @@ export const downloadRestaurantReport = (
   );
 };
 
+export const downloadSubChefRestaurantReport = (
+  fromDate = "",
+  toDate = "",
+  payment = "",
+  section = "",
+  table = "",
+  breakdownOption = ""
+) => {
+  return SERVER.get(
+    `${SUB_CHEF_URL}/download?fromDate=${fromDate}&toDate=${toDate}&payment=${payment}&section=${section}&table=${table}&breakdownOption=${breakdownOption}`
+  );
+};
+
 export const getSubChefRestaurantOrdersPage = (
   page = 1,
   fromDate = "",
   toDate = "",
   payment = "",
   section = "",
-  table = ""
+  table = "",
+  breakdownOption = ""
 ) => {
-  return SERVER.get(`${SUB_CHEF_URL}/restaurant-orders?page=${page}&fromDate=${fromDate}&toDate=${toDate}&payment=${payment}&section=${section}&table=${table}`);
+  return SERVER.get(`${SUB_CHEF_URL}/restaurant-orders?page=${page}&fromDate=${fromDate}&toDate=${toDate}&payment=${payment}&section=${section}&table=${table}&breakdownOption=${breakdownOption}`);
 };
 
 export const getAdminDashboardCrud = (
